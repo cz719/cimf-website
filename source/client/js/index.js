64792c08505ec2f750b2bce1bfa6e377a67701d6
Daiwei <daiweilu123@gmail.com>
2016-01-27 21:51:01 -0500

    Add dropdown animation

--- source/client/js/index.js
+++ source/client/js/index.js
@@ -1,5 +1,32 @@
+'use strict';
+
 (function (window, document) {
 
+  var getStyle = window.getComputedStyle;
 
+  function pxToNumber(str) {
+    return parseInt(str.slice(0, -2));
+  }
+
+  var isDropdownOpen = false;
+
+  function showDropdownClickHandler(event) {
+    event.preventDefault();
+
+    if (!isDropdownOpen) {
+      isDropdownOpen = true;
+      var dropdown = document.getElementById('js-dropdown');
+      var computedStyle = getStyle(dropdown);
+      var marginTop = pxToNumber(computedStyle['margin-top']);
+      var height = pxToNumber(computedStyle['height']);
+      document.getElementById('js-dropdown-mask').style.height = height + 'px';
+    } else {
+      isDropdownOpen = false;
+      document.getElementById('js-dropdown-mask').style.height = '';
+    }
+  }
+
+  document.getElementById('js-show-dropdown')
+    .addEventListener('click', showDropdownClickHandler, false);
 
 })(window, document);
